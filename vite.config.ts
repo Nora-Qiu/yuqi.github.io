e49b5c4ffaa1077dffbd063618964c33478dd258
Nora-Qiu <354398364@qq.com>
2025-11-25 12:44:17 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // TODO: 如果你的 GitHub 仓库名不是 academic-portfolio，请修改这里！
-  // 例如：如果仓库是 'my-web', 这里就填 '/my-web/'
-  // 如果是 'username.github.io', 这里就填 '/'
-  base: '/academic-portfolio/',
+  // Set base to '/' for User Pages (e.g. username.github.io)
+  base: '/',
 })
